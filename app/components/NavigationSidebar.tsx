ac7dbcfda51c6c93f6313fff1f8d0ef3ce013dc2
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-11-20 15:04:05 +0100

    navigator sidebar has links

--- app/components/NavigationSidebar.tsx
+++ app/components/NavigationSidebar.tsx
@@ -1,10 +1,21 @@
+import {NavLink} from '@remix-run/react';
 import type {Category} from '../lib/categories';
 
 export function NavigationSidebar({categories}: {categories: Category[]}) {
   return (
     <div style={{fontSize: 11}}>
       {categories.map((category) => (
-        <div key={category.collectionHandle}>{category.name}</div>
+        <NavLink
+          key={category.collectionHandle}
+          prefetch="intent"
+          to={`/categories/${category.metaobjectHandle}`}
+          style={({isActive}) => ({
+            display: 'block',
+            fontWeight: isActive ? 'bold' : 'normal',
+          })}
+        >
+          {category.name}
+        </NavLink>
       ))}
     </div>
   );
